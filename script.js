4173712236781f420cf6b74d4a50704681bc1d6c
Tatiana Stewart <tstwrt7@gmail.com>
2023-10-04 13:14:59 -0400

    Finished card, adjusted flap size, added media query

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ const card = document.querySelector("#card");
 const message = document.querySelector("#message");
 const images = document.querySelector("#images");
 
+//Refresh Page 
+const refreshButton = document.getElementById('refresh-button');
+
+refreshButton.addEventListener('click', function() {
+    location.reload();
+});
+
 function openEnvelope() {
     envelope.style.transform = "scale(0.7)";
-    envelope.style.marginTop = "50px";
+    envelope.style.marginTop = "60px";
 
     setTimeout(function () {
         flap.classList.toggle("open");
@@ -18,17 +25,16 @@ function openEnvelope() {
     setTimeout(function () {
         card.classList.toggle("open");
         message.style.display = "block";
-        images.style.display = "flex";
+        images.style.display = "block";
         flap.style.display = "none";
-    }, 2000)
+    }, 2000);
+
+    setTimeout(function () {
+        envelope.style.display = "none";
+        refreshButton.classList.toggle("open");
+    }, 13000)
 }
 
 openEnvelopeButton.addEventListener('click', openEnvelope);
 
 
-//Refresh Page 
-const refreshButton = document.getElementById('refresh');
-
-refreshButton.addEventListener('click', function() {
-    location.reload();
-});
